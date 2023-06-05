8781c61baa843bae0c8a10b041b5b03b8cee44ec
Shahabr99 <sean.rohani9@gmail.com>
2023-06-04 22:33:59 -0700

    user model test completed

--- test_user_model.py
+++ test_user_model.py
@@ -36,11 +36,27 @@ class UserModelTestCase(TestCase):
     def setUp(self):
         """Create test client, add sample data."""
 
-        User.query.delete()
-        Message.query.delete()
-        Follows.query.delete()
-
+        db.drop_all()
+        db.create_all()
+        u1 = User.signup("test1", "email1@email.com", "password", None)
+        uid1 = 1111
+        u1.id = uid1
+        u2 = User.signup("test2", "email2@email.com", "password", None)
+        uid2 = 2222
+        u2.id = uid2
+        db.session.commit()
+        u1 = User.query.get(uid1)
+        u2 = User.query.get(uid2)
+        self.u1 = u1
+        self.uid1 = uid1
+        self.u2 = u2
+        self.uid2 = uid2
         self.client = app.test_client()
+
+    def tearDown(self):
+        res = super().tearDown()
+        db.session.rollback()
+        return res
 
     def test_user_model(self):
         """Does basic model work?"""
@@ -58,26 +74,70 @@ class UserModelTestCase(TestCase):
         self.assertEqual(len(u.messages), 0)
         self.assertEqual(len(u.followers), 0)
 
-<<<<<<< HEAD
-=======
 
->>>>>>> fb05c5b (adding the tests)
     def test_repr(self):
         """Testing the __repr__ dunder method"""
-        test_user = User(username="test1", email="test@gmail.com", password='test123')
-        result = repr(test_user)
-        expected = "User 1: test1, test@gmail.com"
-        self.assertEqual(result, expected)
-<<<<<<< HEAD
         
-=======
+        result = repr(self.u1)
+        expected = "User 1: test1, email@email.com"
+        self.assertEqual(result, expected)
 
 
     def test_is_following(self):
         """Testing if a user1 follows user2"""
-        user1 = User(username='test1', email="test@gmail.com", password="test123")
-        user2 = User(username='test2', email="test2@gmail.com", password="test321")
+        self.u1.following.append(self.u2)
+        db.session.commit()
 
-        found_user_list = [user for user in self.following if user == user2]
+        self.assertTrue(self.u1.is_following(self.u2))
+        self.assertFalse(self.u2.is_following(self.u1))
 
->>>>>>> fb05c5b (adding the tests)
+
+    def test_is_followed_by(self):
+        """Testing if user1 is followed  by user2"""
+        self.u1.followers.append(self.u2)
+        db.session.commit()
+
+        self.assertTrue(self.u2.followers(self.u1))
+        self.assertFalse(self.u1.followers(self.u2))
+
+
+    def test_valid_signup(self):
+        """Testing creating a new user when a user signsup"""
+        user = User.signup('user1', 'user1@email.com', 'Password', None)
+        uid = 5555
+        user.id = uid
+        deb.session.commit()
+        test_user = User.query.get(uid)
+        self.assertIsNotNone(user)
+        self.assertEqual(user.username, "user1")
+        self.assertEqual(user.email, "user1@email.com")
+        self.assertNotEqual(user1.password, "Passwod")
+        self.assertTrue(user1.password.startswith("$2b$"))
+
+
+    def test_invalid_username_signup(self):
+        invalid = User.signup(None, "test@test.com", "password", None)
+        uid = 12345678
+        invalid.id = uid
+        with self.assertRaises(exc.IntegrityError) as context:
+            db.session.commit()
+
+
+    def test_invalid_email_signup(self):
+        test_user = User.signup('test_user', None, "password", None)
+        uid = 12456789
+        test_user.id = uid
+        with self.assertRaises(exc.IntegrityError) as context:
+            db.session.commit()
+    
+    def test_valid_authentication(self):
+        u = User.authenticate(self.u1.username, "password")
+        self.assertIsNotNone(u)
+        self.assertEqual(u.id, self.uid1)
+
+    def test_invalid_username(self):
+        self.assertFalse(User.authenticate("wrongusername", "password"))
+
+
+    def test_wrong_password(self):
+        self.assertFalse(User.authenticate(self.u1.username, "wrongpassword"))
