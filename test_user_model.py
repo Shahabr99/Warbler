c92f1df335e77fd17792eb7a5352f6ceb8c7f42e
Shahabr99 <sean.rohani9@gmail.com>
2023-06-04 19:00:01 -0700

    still adding tests

--- test_user_model.py
+++ test_user_model.py
@@ -58,10 +58,26 @@ class UserModelTestCase(TestCase):
         self.assertEqual(len(u.messages), 0)
         self.assertEqual(len(u.followers), 0)
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> fb05c5b (adding the tests)
     def test_repr(self):
         """Testing the __repr__ dunder method"""
         test_user = User(username="test1", email="test@gmail.com", password='test123')
         result = repr(test_user)
         expected = "User 1: test1, test@gmail.com"
         self.assertEqual(result, expected)
+<<<<<<< HEAD
         
+=======
+
+
+    def test_is_following(self):
+        """Testing if a user1 follows user2"""
+        user1 = User(username='test1', email="test@gmail.com", password="test123")
+        user2 = User(username='test2', email="test2@gmail.com", password="test321")
+
+        found_user_list = [user for user in self.following if user == user2]
+
+>>>>>>> fb05c5b (adding the tests)
